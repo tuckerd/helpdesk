1298bc12b9063538454f8d98d0e23f4574e370fd
D Tucker <danielle@tuckerlabs.com>
2013-08-22 22:03:58 -0700

    Recreate app secret token and hide in env file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Helpdesk::Application.config.secret_token = 'b8fa8f775737fd527e00b07781f417aed19bf2b3696e97a769db0b067bc3f575f2e18eae1aa526ee02fbf5f625ce1a5c23f10f9ff539058c25381e9a27e21739'
+Helpdesk::Application.config.secret_token = ENV['APP_SECRET_TOKEN']
